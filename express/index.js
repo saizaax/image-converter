03afc3328b82562f37b3138ae619ca0e76c66e04
saizaax <flamesuspect@gmail.com>
2021-10-18 23:43:45 +0300

    Added Express.js backend service.

---
+++ express/index.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const cors = require("cors")
+const jimp = require("jimp")
+const multer = require("multer")
+const { Readable } = require("stream")
+
+const app = express()
+app.use(cors())
+
+app.post("/convert", multer().single("image"), async (req, res) => {
+  const { file, query } = req
+  const { format } = query
+
+  if (!file || !format) {
+    res.status(400)
+    return res.json({ error: "File and target file-format are required." })
+  }
+
+  const { buffer } = file
+
+  jimp
+    .read(buffer)
+    .then((image) => image.getBufferAsync(format))
+    .then((buffer) => {
+      const stream = new Readable()
+      stream.push(buffer)
+      stream.push(null)
+
+      stream.pipe(res)
+    })
+})
+
+app.use((req, res) => {
+  return res.status(404).json({ error: "Not found" })
+})
+
+app.listen(5500, () => {
+  console.log(`Server is listening to port: ${5500}`)
+})
